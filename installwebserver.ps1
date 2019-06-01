9d9079720de3b6611fc32ad2a6451974845c6214
Andrew B Campbell <acampb@softchoice.com>
2019-06-01 14:07:54 -0400

    updated script

--- installwebserver.ps1
+++ installwebserver.ps1
@@ -1,1 +1,5 @@
-Write-Output 'hello world'
+Install-WindowsFeature Web-Server
+
+$htmlcode = "<body style='background-color:orange;'> <br> <center> <h1> <font face='segoe ui light'> DevOps is Awesome! <br><br> <3 Softchoice </font> </h1> </center> </body>"
+
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $htmlcode
